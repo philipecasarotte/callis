5ce1a0782bdfbb2c0c19656ad93ca7ed7c5a547f
Philipe Casarotte <phisoccer@globo.com>
2010-06-17 17:37:54 -0300

    nts fix order

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,5 +6,5 @@ class Event < ActiveRecord::Base
   acts_as_seo
   
   named_scope :upcoming, :conditions => ["happens_on >= ?", Date.today]
-  named_scope :past, :conditions => ["happens_on < ?", Date.today], :order => "DESC"
+  named_scope :past, :conditions => ["happens_on < ?", Date.today], :order => ["happens_on DESC"]
 end
