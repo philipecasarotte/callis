4a69b2b642e1641854b7c5eb679ad6956bc950ca
Philipe Casarotte <philipetauk@gmail.com>
2015-07-09 18:53:04 -0700

    dasd

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   # config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => 'http://gems.github.com'
   config.gem "justinfrench-formtastic", :version => ">=0.2.2", :lib => 'formtastic', :source  => 'http://gems.github.com'
-
+  config.gem 'capistrano', '~> 2.15'
+  
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
